05cc6f201dbb77ba2369547e3536f6e0e44d6262
github username <github email address>
2018-04-27 08:59:10 -0400

    making delete

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.getElementById("infoWrap").addEventListener('submit', addInfo)
 
-  document.getElementById("infoWrap").addEventListener('submit', addInfo)
+let counter = 0
+function addInfo(){
+  event.preventDefault()
+  let nameInput = document.getElementById('nameInput').value
+  let textInput = document.getElementById('textInput').value
+  catchAll.push([counter, nameInput, textInput])
+  counter++
 
-  function addInfo(){
-    event.preventDefault()
-    let nameInput = document.getElementById('nameInput').value
-    let textInput = document.getElementById('textInput').value
-
-    document.getElementById('demo').innerHTML += (`
+  document.getElementById('demo').innerHTML += (`
+    <div id="post-${counter}" class='wrapper'>
       <h2>${nameInput}</h2>
       <h3>${textInput}</h3>
-    `)
+      <button data-postid='${counter}' type="button" name="button">delete</button>
+    </div>
+  `)
 
-      // Adapter.pushInfo(nameInput, textInput)
+  document.getElementById('demo').addEventListener('click', function(){
+      let idToDelete = event.target.dataset.postid
+      // document.getElementById(`post-${idToDelete}`).remove()
+      // console.log(catchAll)
+      document.getElementById(`demo`).innerHTML = ''
+      catchAll = catchAll.filter(x => x != idToDelete)
 
+      catchAll.forEach(x => {
+          document.getElementById('demo').innerHTML += (`
+            <div id="post-${x[0]}" class='wrapper'>
+            <h2>${x[1]}</h2>
+            <h3>${x[2]}</h3>
+            <button data-postid='${x}' type="button" name="button">delete</button>
+            </div>`)
+    })
+})
       document.getElementById('nameInput').value = ''
       document.getElementById('textInput').value = ''
+}
+    // Adapter.pushInfo(nameInput, textInput)
 
+let catchAll = []
 
-    document.getElementById('demo').innerHTML += (`
-      <h2>${document.getElementById('nameInput').value}</h2>
-      <h3>${document.getElementById('textInput').value}</h3>
-    `)
-
-  }
-})
-
-
-// build out a news feed with a name and comments from different users make it pessemisticlly
+// push info to array and rerender after deleteing something
